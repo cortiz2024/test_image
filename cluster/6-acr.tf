c4bb53783b59cc2e12ca9550bdae06d3233e3932
Cesar Ortiz <anortiz@andreani.com>
2024-01-22 17:40:02 -0500

    test deploy

--- cluster/6-acr.tf
+++ cluster/6-acr.tf
@@ -1,7 +1,7 @@
 
 
 resource "azurerm_container_registry" "acr" {
-  name                = "${local.aks_name}${local.env}acr"
+  name                = "${local.aks_name}${local.env}acr1"
   resource_group_name = local.resource_group_name
   location            = local.region
   sku                 = "Standard"
