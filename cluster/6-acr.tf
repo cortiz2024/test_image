3fb2dfb49598dfe79a55e5d46606970a46c3de2c
Cesar Ortiz <anortiz@andreani.com>
2024-01-25 10:13:17 -0500

    Changed ACR to GHCR

--- cluster/6-acr.tf
+++
@@ -1,6 +0,0 @@
-resource "azurerm_container_registry" "acr" {
-  name                = "${local.aks_name}${local.env}acr1"
-  resource_group_name = local.resource_group_name
-  location            = local.region
-  sku                 = "Standard"
-}
